42d019febdb5bb72550ad319da83ae40fe634a6a
Catman06 <zippercat06@gmail.com>
2025-05-21 15:30:11 -0500

    Made the app have multiple entry points for the different pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        blog: resolve(__dirname, 'html/blog.html'),
+        resume: resolve(__dirname, 'html/resume.html')
+      }
+    }
+  }
 })
