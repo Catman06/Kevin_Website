bba81cd53aef7b093acbe3163af93a27c9f9f1bd
Catman06 <zippercat06@gmail.com>
2025-07-21 07:16:52 -0500

    Removed service worker to use browser cache instead

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,13 @@ import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { VitePWA } from 'vite-plugin-pwa';
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    VitePWA({
-      srcDir: "src/sw",
-      filename: "cacheServiceWorker.ts",
-      strategies: "injectManifest",
-      injectRegister: false,
-      manifest: false,
-      injectManifest: {
-        injectionPoint: undefined,
-      },
-      devOptions: {
-        enabled: true
-      },
-    })
+    vue()
   ],
   appType: 'mpa',
   build: {
