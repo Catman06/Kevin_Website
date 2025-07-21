423cc7dffdd0273af98103997c060202a5015cdd
Catman06 <zippercat06@gmail.com>
2025-07-21 10:03:16 -0500

    Fixed PostMeta's RegEx

--- public/php/PostMeta.php
+++ public/php/PostMeta.php
@@ -5,10 +5,10 @@ try {
 	$path = "/srv/http/nginx/posts/$filename.html";
 	// Get the meta information from the file
 	$content = file_get_contents($path);
-	preg_match('/<meta name="date".*content="(.+).*\/>/im', $content, $date);
-	preg_match('/<meta name="categories".*content="(.+).*\/>/im', $content, $categories);
-	preg_match('/<meta name="title".*content="(.+).*\/>/im', $content, $title);
-	preg_match('/<meta name="blurb".*content="(.+).*\/>/im', $content, $blurb);
+	preg_match('/<meta name="date".*content="(.+)".*\/>/im', $content, $date);
+	preg_match('/<meta name="categories".*content="(.+)".*\/>/im', $content, $categories);
+	preg_match('/<meta name="title".*content="(.+)".*\/>/im', $content, $title);
+	preg_match('/<meta name="blurb".*content="(.+)".*\/>/im', $content, $blurb);
 	// Add all the acquired info fields to the response
 	$response += ["date" => $date[1], "categories" => $categories[1], "title" => $title[1], "blurb" => $blurb[1]];
 } catch (Exception $ex) {
