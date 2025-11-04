b68e2c3e4d4b963af56cc503acabf1364601a6c0
Catman06 <zippercat06@gmail.com>
2025-11-04 08:16:10 -0600

    Make php paths relative

--- public/php/PostMeta.php
+++ public/php/PostMeta.php
@@ -2,18 +2,27 @@
 header("Cache-Control: max-age=86400");
 $response = [];
 try {
-	$filename = $_GET["url"];
-	$path = "/srv/http/nginx/posts/$filename.html";
-	// Get the meta information from the file
-	$content = file_get_contents($path);
-	preg_match('/<meta name="date".*content="(.+)".*\/>/im', $content, $date);
-	preg_match('/<meta name="categories".*content="(.+)".*\/>/im', $content, $categories);
-	preg_match('/<meta name="title".*content="(.+)".*\/>/im', $content, $title);
-	preg_match('/<meta name="blurb".*content="(.+)".*\/>/im', $content, $blurb);
-	// Add all the acquired info fields to the response
-	$response += ["date" => $date[1], "categories" => explode(',', $categories[1]), "title" => $title[1], "blurb" => $blurb[1]];
+    $filename = $_GET["url"];
+    $path = "../posts/$filename.html";
+    // Get the meta information from the file
+    $content = file_get_contents($path);
+    preg_match('/<meta name="date".*content="(.+)".*\/>/im', $content, $date);
+    preg_match(
+        '/<meta name="categories".*content="(.+)".*\/>/im',
+        $content,
+        $categories,
+    );
+    preg_match('/<meta name="title".*content="(.+)".*\/>/im', $content, $title);
+    preg_match('/<meta name="blurb".*content="(.+)".*\/>/im', $content, $blurb);
+    // Add all the acquired info fields to the response
+    $response += [
+        "date" => $date[1],
+        "categories" => explode(",", $categories[1]),
+        "title" => $title[1],
+        "blurb" => $blurb[1],
+    ];
 } catch (Exception $ex) {
-	$response["error"] = $ex;
+    $response["error"] = $ex;
 }
 
 echo json_encode($response);
